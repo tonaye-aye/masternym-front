f9b4c3f6bce0c272b03d9b0ea281abe28875d5e5
Tony Gorgioski <tony.gorgioski@gmail.com>
2020-09-10 18:16:28 +1000

    form testing

--- src/pages/Contactus.vue
+++ src/pages/Contactus.vue
@@ -58,22 +58,22 @@ export default {
   name: "Contact",
   data() {
     return {
-      valid: true,
-      name: "",
-      nameRules: [
-        (v) => !!v || "Name is required",
-        (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
-      ],
-      email: "",
-      emailRules: [
-        (v) => !!v || "E-mail is required",
-        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
-      ],
-      message: "",
-      messageRules: [
-        (v) => !!v || "Message is required",
-        (v) => (v && v.length <= 250) || "Send us your thoughts and feedback",
-      ],
+      // valid: true,
+      // name: "",
+      // nameRules: [
+      //   (v) => !!v || "Name is required",
+      //   (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+      // ],
+      // email: "",
+      // emailRules: [
+      //   (v) => !!v || "E-mail is required",
+      //   (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      // ],
+      // message: "",
+      // messageRules: [
+      //   (v) => !!v || "Message is required",
+      //   (v) => (v && v.length <= 250) || "Send us your thoughts and feedback",
+      // ],
       formData: {},
     };
   },
