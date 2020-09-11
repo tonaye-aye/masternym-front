7f9adbc52195aa3459c82a13cff4479d1a39b9bc
Tony Gorgioski <tony.gorgioski@gmail.com>
2020-09-11 15:40:39 +1000

    added tooltips

--- src/templates/Acronym.vue
+++ src/templates/Acronym.vue
@@ -3,10 +3,16 @@
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
         <v-col cols="12" sm="8" md="4">
-          <p class="text-h5 font-weight-light grey--text mb-5">/acronyms/{{ $page.acronym.short }}</p>
+          <p class="text-h5 font-weight-light grey--text mb-5">
+            /acronyms/{{ $page.acronym.short }}
+          </p>
           <v-card>
-            <v-card-title class="text-h3">{{ $page.acronym.short }}</v-card-title>
-            <v-card-subtitle class="text-overline font-weight-light">{{ $page.acronym.long }}</v-card-subtitle>
+            <v-card-title class="text-h3">{{
+              $page.acronym.short
+            }}</v-card-title>
+            <v-card-subtitle class="text-overline font-weight-light">{{
+              $page.acronym.long
+            }}</v-card-subtitle>
             <v-card-text>
               <blockquote class="blockquote black--text orange accent-2">
                 <div class="text-caption font-italic mb-1">Example:</div>
@@ -16,12 +22,22 @@
             <v-card-actions>
               <v-btn text color="orange" @click="$router.push(`/`)">Back</v-btn>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn icon v-bind="attrs" v-on="on">
+                    <v-icon>mdi-heart</v-icon>
+                  </v-btn>
+                </template>
+                <span>'Favourite' coming soon</span>
+              </v-tooltip>
+              <v-tooltip bottom open-on-click>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn icon v-bind="attrs" v-on="on">
+                    <v-icon>mdi-share-variant</v-icon>
+                  </v-btn>
+                </template>
+                <span>'Share' coming soon</span>
+              </v-tooltip>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -45,8 +61,8 @@ query ($id: ID!) {
 export default {
   metaInfo() {
     return {
-      title: this.$page.acronym.short,
+      title: this.$page.acronym.short
     };
-  },
+  }
 };
 </script>
